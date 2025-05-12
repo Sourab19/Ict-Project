9e6869b097baa232e1146f20579ac4fcd2cb035b
Sourab19 <sourabberlin007@gmail.com>
2025-05-12 09:39:23 +0530

    change in app.js

--- project_backend/app.js
+++ project_backend/app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: ['https://ict-project-2.vercel.app/'],
+    origin: ['https://ict-project-2.vercel.app'],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ['Content-Type', 'Authorization','token'],
